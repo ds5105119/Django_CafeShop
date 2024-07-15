d9670ca25fb331546ae63c0d4f26bd5be6d033a7
최규성 <76863081+Choi9912@users.noreply.github.com>
2024-07-15 15:42:57 +0900

    Update admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Product, Post, Point, Order, Category, Cart
+from .models import Product, Point, Order, Category, Cart
 # Register your models here.
 
 admin.site.register(Product)
-admin.site.register(Post)
 admin.site.register(Point)
 admin.site.register(Order)
 admin.site.register(Category)
